97324069f237083db9d3b83f1d0fb8ee7966c899
Yesid Gonzalez <oquintero@openmytab.com>
2022-10-26 00:55:16 -0500

    ADD - the models referring to the database are created

---
+++ src/models/contact_model.py
@@ -0,0 +1,18 @@
+from tortoise.fields import CharField, DateField, TextField
+from tortoise.fields.relational import ForeignKeyField, ReverseRelation
+
+from src.models.base_model import BaseModel
+
+class ContactModel(BaseModel):
+    name = CharField(max_length=100)
+    lastname = CharField(max_length=100, null=True)
+    email = CharField(max_length=150, null=True)
+    birthday = DateField(null=True)
+    photo = TextField(null=True)
+
+    user = ForeignKeyField('models.UserModel', related_name='contact_user_fk')
+
+    phones: ReverseRelation['PhoneModel']
+
+    class Meta:
+        table = 'contacts'
